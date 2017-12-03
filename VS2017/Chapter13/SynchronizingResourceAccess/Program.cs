b79289b2a586f6bae923a2d9d4cf936bae1bac38
Mark J Price <markjprice@gmail.com>
2017-12-03 14:29:00 +0000

    Final draft

---
+++ VS2017/Chapter13/SynchronizingResourceAccess/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static System.Console;
+
+namespace SynchronizingResourceAccess
+{
+    class Program
+    {
+        static Random r = new Random();
+        static string Message; // a shared resource 
+
+        static object conch = new object();
+
+        static void MethodA()
+        {
+            lock (conch)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "A";
+                    Write(".");
+                }
+            }
+        }
+
+        static void MethodB()
+        {
+            lock (conch)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(r.Next(2000));
+                    Message += "B";
+                    Write(".");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLine("Please wait for the tasks to complete.");
+            Stopwatch watch = Stopwatch.StartNew();
+
+            Task a = Task.Factory.StartNew(MethodA);
+            Task b = Task.Factory.StartNew(MethodB);
+
+            Task.WaitAll(new Task[] { a, b });
+            WriteLine();
+            WriteLine($"Results: {Message}.");
+            WriteLine($"{watch.ElapsedMilliseconds:#,##0} elapsed milliseconds.");
+        }
+    }
+}
