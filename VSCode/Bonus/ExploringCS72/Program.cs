58cd56839d87971056191ac13bd5f54437f4a2c5
Mark J Price <markjprice@gmail.com>
2018-01-08 08:01:51 +0000

    Bonus code
    Solution for a bonus article online covering C# 7.2 features

---
+++ VSCode/Bonus/ExploringCS72/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace ExploringCS72
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 0b_0000_0111_1011_0100;
+            WriteLine($"I was born in {year}.");
+
+            PassingParameters(name: "Bob", 1945);
+        }
+
+        public void ReadFieldsInDerivedType()
+        {
+            WriteLine("Inside a type in different assembly:");
+            var am = new AccessModifiers();
+            WriteLine(am.Everywhere);
+        }
+
+        public static void PassingParameters(string name, int year)
+        {
+            WriteLine($"{name} was born in {year}.");
+        }
+    }
+    
+    public class DerivedInDifferentAssembly : AccessModifiers
+    {
+        public void ReadFieldsInDerivedType()
+        {
+            WriteLine("Inside a derived type in different assembly:");
+            WriteLine(InDerivedType);
+            WriteLine(InSameAssemblyOrDerivedType);
+            WriteLine(Everywhere);
+        }
+    }
+
+}
---
+++ VSCode/Bonus/ExploringCS72Lib/AccessModifiers.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace ExploringCS72
+{
+    public class AccessModifiers
+    {
+        private int InTypeOnly;
+        internal int InSameAssembly;
+        protected int InDerivedType;
+        internal protected int InSameAssemblyOrDerivedType;
+        private protected int InSameAssemblyAndDerivedType; // C# 7.2
+        public int Everywhere;
+
+        public void ReadFields()
+        {
+            WriteLine("Inside the same type:");
+            WriteLine(InTypeOnly);
+            WriteLine(InSameAssembly);
+            WriteLine(InDerivedType);
+            WriteLine(InSameAssemblyOrDerivedType);
+            WriteLine(InSameAssemblyAndDerivedType);
+            WriteLine(Everywhere);
+        }
+    }
+
+    public class DerivedInSameAssembly : AccessModifiers
+    {
+        public void ReadFieldsInDerivedType()
+        {
+            WriteLine("Inside a derived type in same assembly:");
+            //WriteLine(InTypeOnly); // is not visible
+            WriteLine(InSameAssembly);
+            WriteLine(InDerivedType);
+            WriteLine(InSameAssemblyOrDerivedType);
+            WriteLine(InSameAssemblyAndDerivedType);
+            WriteLine(Everywhere);
+        }
+    }
+}
